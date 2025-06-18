81c025226397ed7010dee1c3261c597e0fe76578
Enzo Oliveira da Rocha <Enzo25rocha@gmail.com>
2025-06-17 21:20:55 -0300

    wip: adding expiration time + some other security measures through libs

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -19,6 +19,19 @@ from django.urls import path, include
 from django.conf import settings
 from wheater.views import WeatherLocationView
 from accounts.views import UserFavoriteLocationsView, UserBootLocationView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+"""
+Armazene o access token (com cuidado!) e o refresh token
+
+Quando o access expirar, chame api/token/refresh/ com o refresh token para pegar um novo access
+
+Faça logout se o refresh também expirar
+
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +41,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('search-city/', WeatherLocationView.as_view(), name='Search_City'),
     path('favorite_locations/', UserFavoriteLocationsView.as_view(), name='user_favorite_locations'),
-    path('boot_location/', UserBootLocationView.as_view(), name='user_boot_location')
+    path('boot_location/', UserBootLocationView.as_view(), name='user_boot_location'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
  
